aa2a9cf6f209420cf3e326b6072c764a583799ac
Jonathan <36110242+DrFineSir@users.noreply.github.com>
2022-01-16 00:26:47 -0500

    Update [...auth].ts

--- app/api/auth/[...auth].ts
+++ app/api/auth/[...auth].ts
@@ -13,7 +13,7 @@ export default passportAuth({
         {
           clientID: process.env.AUTH_GOOGLE_CLIENTID,
           clientSecret: process.env.AUTH_GOOGLE_CLIENTSECRET,
-          callbackURL: process.env.HOST_URL,
+          callbackURL: `${process.env.HOST_URL}/api/auth/google/callback`,
           scope: googleScopes,
         },
         async function (accessToken, refreshToken, profile, done) {
